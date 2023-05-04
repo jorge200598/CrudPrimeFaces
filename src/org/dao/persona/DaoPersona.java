a26701e33ca2b23a5f59373fd9a3d3f612e4eef8
ING.Jorge Duarte Huanosto <huanosto3002255@gmail.com>
2023-05-04 10:42:54 -0600

    CREACION DE PROYECTO

---
+++ src/org/dao/persona/DaoPersona.java
@@ -0,0 +1,102 @@
+package org.dao.persona;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.modelo.persona.Persona;
+	@SuppressWarnings("serial")
+	public class DaoPersona implements Serializable{
+			//INSERTAR
+		public void addPersona(Persona persona) {
+			AnnotationConfiguration cfg = new AnnotationConfiguration();
+			cfg.configure("hibernate.cfg.xml");
+			SessionFactory factory = cfg.buildSessionFactory();
+			Session session = factory.getCurrentSession();
+			try {
+				
+				session.beginTransaction();
+				session.save(persona);
+				session.getTransaction().commit();
+				
+			} catch (Exception e) {
+				System.out.println("Exception : " + e.getMessage());
+				
+			}
+		}
+
+			//ACTUALIZAR
+		
+		public void updatePersona(Persona persona) {
+			AnnotationConfiguration cfg = new AnnotationConfiguration();
+			cfg.configure("hibernate.cfg.xml");
+			SessionFactory factory = cfg.buildSessionFactory();
+			Session session = factory.getCurrentSession();
+			try {
+				
+				session.beginTransaction();
+				session.update(persona);
+				session.getTransaction().commit();
+				
+			} catch (Exception e) {
+				System.out.println("Exception : " + e.getMessage());
+				
+			}
+		}
+		//ELIMINAR
+		public void daletePersona(Persona persona) {
+			AnnotationConfiguration cfg = new AnnotationConfiguration();
+			cfg.configure("hibernate.cfg.xml");
+			SessionFactory factory = cfg.buildSessionFactory();
+			Session session = factory.getCurrentSession();
+			try {
+				
+				session.beginTransaction();
+				session.delete(persona);
+				session.getTransaction().commit();
+				
+			} catch (Exception e) {
+				System.out.println("Exception : " + e.getMessage());
+				
+			}
+		}
+		
+		//LISTAR PERSONAS
+		@SuppressWarnings("unchecked")
+		public List<Persona> ListPersona(){
+			AnnotationConfiguration cfg = new AnnotationConfiguration();
+			cfg.configure("hibernate.cfg.xml");
+			SessionFactory factory = cfg.buildSessionFactory();
+			Session session = factory.getCurrentSession();
+			List<Persona> list = null;
+			try {
+				
+				session.beginTransaction();
+				list = (List<Persona>) session.createQuery("from Persona").list();
+				session.getTransaction().commit();
+				
+			} catch (Exception e) {
+				System.out.println("Exception : " + e.getMessage());
+			}
+			return list;
+		}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/org/modelo/persona/Persona.java
@@ -0,0 +1,62 @@
+package org.modelo.persona;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@SuppressWarnings("serial")
+@Entity
+public class Persona implements Serializable{
+	private Integer id;
+	private String nombre;
+	private String telefono;
+	
+	//SUPER CLASS
+	
+	public Persona() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	//FIELDS DE TODOS
+	public Persona(Integer id, String nombre, String telefono) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.telefono = telefono;
+	}
+	
+	@Id
+	@GeneratedValue
+	
+	//GETTERS AND SETTERS DE TODOS
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+
+
+}
